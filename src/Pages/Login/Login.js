b458ac7a8a2941bb27479044f213904364567246
arafathossion <arafathossionrahman@gmail.com>
2022-05-08 23:07:53 +0600

    work on login component

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 import { Col, Container, Row, Tab, Tabs, Form, Button } from 'react-bootstrap';
 import { useAuthState, useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../../Components/Loading/Loading';
 import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
     // const [user] = useAuthState(auth);
-    const [key, setKey] = useState('singIn');
+    const [key, setKey] = useState('signIn');
     const navigate = useNavigate();
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
@@ -17,56 +18,63 @@ const Login = () => {
         users,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
     const [
         signInWithEmailAndPassword,
-        singInUser,
-        singInLoading,
-        singInError,
+        signInUser,
+        signInLoading,
+        signInError,
     ] = useSignInWithEmailAndPassword(auth);
 
 
 
-    const handleSingUp = async (e) => {
+    const handlesignUp = async (e) => {
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         await createUserWithEmailAndPassword(email, password)
-       
+
 
     }
 
-    const [signInWithGoogle, googlesingInuser, googlesingInloading, googlesingInerror] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googlesignInuser, googlesignInloading, googlesignInerror] = useSignInWithGoogle(auth);
 
 
 
     // const [token] = useToken(user);
-    
-    if (users || singInUser || googlesingInuser) {
+
+    if (users || signInUser || googlesignInuser) {
         navigate(from, { replace: true });
     }
 
+    if(loading || signInLoading){
+        return <Loading></Loading>
+      }
 
-    if (googlesingInerror) {
-        console.log(googlesingInerror?.message)
+
+
+    let errorElemetn;
+    if (googlesignInerror || signInError) {
+        errorElemetn = <p>Error: {googlesignInerror?.message} {signInError?.message}</p>
     }
 
 
 
-    const handleSingIn = async (e) => {
+
+    const handlesignIn = async (e) => {
         e.preventDefault();
-        const singInEmail = e.target.email.value;
-        const singInPassword = e.target.password.value;
-        await signInWithEmailAndPassword(singInEmail, singInPassword)
-        console.log(singInEmail, singInPassword)
+        const signInEmail = e.target.email.value;
+        const signInPassword = e.target.password.value;
+        await signInWithEmailAndPassword(signInEmail, signInPassword)
+        console.log(signInEmail, signInPassword)
         //  const url = `https://still-fjord-04706.herokuapp.com/login`;
-        const { data } = await axios.post('https://still-fjord-04706.herokuapp.com/login', { singInEmail });
-                localStorage.setItem('accessToken', data.accessToken);
-        navigate(from, { replace: true });
+        const { data } = await axios.post('https://still-fjord-04706.herokuapp.com/login', { signInEmail });
+        localStorage.setItem('accessToken', data.accessToken);
+        // navigate(from, { replace: true });
     }
-   
+
 
 
     return (
@@ -79,15 +87,15 @@ const Login = () => {
                         onSelect={(k) => setKey(k)}
                         className="mb-3"
                     >
-                        <Tab eventKey="singUp" title="Sing Up">
-                            <h6>Sing Up</h6>
-                            <Form onSubmit={handleSingUp}>
-                                <Form.Group className="mb-3" controlId="singupname">
+                        <Tab eventKey="signUp" title="Sign Up">
+                            <h6>sign Up</h6>
+                            <Form onSubmit={handlesignUp}>
+                                <Form.Group className="mb-3" controlId="signupname">
                                     <Form.Label>User Name</Form.Label>
                                     <Form.Control type="text" name='name' placeholder="User Name" />
                                 </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="singupemail">
+                                <Form.Group className="mb-3" controlId="signupemail">
                                     <Form.Label>Email address</Form.Label>
                                     <Form.Control type="email" name='email' placeholder="Enter email" />
                                     <Form.Text className="text-muted">
@@ -95,20 +103,21 @@ const Login = () => {
                                     </Form.Text>
                                 </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="singuppassword">
+                                <Form.Group className="mb-3" controlId="signuppassword">
                                     <Form.Label>Password</Form.Label>
                                     <Form.Control type="password" name='password' placeholder="Password" />
                                 </Form.Group>
-                                <Form.Group className="mb-3" controlId="singupcheckbox">
+                                <Form.Group className="mb-3" controlId="signupcheckbox">
                                     <Form.Check type="checkbox" label="Check me out" />
                                 </Form.Group>
                                 <Button variant="primary" type="submit">
                                     Submit
                                 </Button>
+
                             </Form>
                         </Tab>
-                        <Tab eventKey="singIn" title="Sing In">
-                            <Form onSubmit={handleSingIn}>
+                        <Tab eventKey="signIn" title="Sign In">
+                            <Form onSubmit={handlesignIn}>
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Label>Email address</Form.Label>
                                     <Form.Control type="email" name='email' placeholder="Enter email" />
@@ -128,6 +137,7 @@ const Login = () => {
                                     Submit
                                 </Button>
                             </Form>
+                            <h5 className="text-danger pt-md-3 pt-1">{errorElemetn}</h5>
                             <button onClick={() => signInWithGoogle()}>Sign In</button>
                         </Tab>
 
